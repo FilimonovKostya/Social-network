1bb489f980787d4ce1c99fceac53cfea4d7b7f1d
Konstantin Filimonov <58990191+FilimonovKostya@users.noreply.github.com>
2020-08-24 11:18:13 +0300

    make layout

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+    return <header className={'header'}>
+        <img src="https://cdn.worldvectorlogo.com/logos/react-2.svg" alt="logo"/>
+    </header>
+}
+
+export default Header
---
+++ src/Components/Profile/Profile.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Profile() {
+    return <main className={'profile'}>
+        <div><img src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fjosephliu%2Ffiles%2F2019%2F06%2F1-office-1516329_1920-1200x299.jpg'
+                  alt="bg"/></div>
+        <div>Ava + des</div>
+        <div>My posts</div>
+        <div>New post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+    </main>
+}
+
+export default Profile
