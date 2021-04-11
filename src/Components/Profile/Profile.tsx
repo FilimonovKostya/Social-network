8809cb565fac69a77daad3aa8bfab68c99d4047e
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-04-11 21:00:16 +0300

    add profile

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import style from './Profile.module.css'
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import Posts from "./Posts/Posts";
 
 const Profile = () => {
     return <main className={style.profile}>
-        <div><img
-            src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fjosephliu%2Ffiles%2F2019%2F06%2F1-office-1516329_1920-1200x299.jpg'
-            alt="bg"/></div>
-        <div>Ava + des</div>
-        <div>My posts</div>
-        <div>New post</div>
-        <div>post 1</div>
-        <div>post 2</div>
+        <ProfileInfo/>
+        <Posts/>
     </main>
 }
+
 export default Profile
---
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import style from './ProfileInfo.module.css'
+
+const ProfileInfo = () => {
+    return <>
+        <div><img className={style.backgroundImage}
+                  src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fjosephliu%2Ffiles%2F2019%2F06%2F1-office-1516329_1920-1200x299.jpg'
+                  alt="bg"/></div>
+        <div className={style.wrapperPersonalInfo}>
+            <img className={style.avatarProfile} src="https://cdn4.iconfinder.com/data/icons/diversity-v2-0-volume-03/64/superhero-spiderman-comics-256.png"
+                 alt=""/>
+            <div className={style.personalInfo}><h3>Filimonov Kostya</h3>
+                <p>Date of birth: <span>21 01 1997</span></p>
+                <p>Education : <span>Polotsk state university</span></p>
+                <p>Web Site: <span>@example.com</span></p>
+            </div>
+        </div>
+
+    </>
+}
+
+export default ProfileInfo
