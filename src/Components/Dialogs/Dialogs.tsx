cef3288fbeeb5632396fd60aadd3626b1c63932b
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-04-12 22:21:43 +0300

    21.Layout Dialogs(not finish)

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import style from './Dialogs.module.css'
 
 const Dialogs = () => {
-   return <div>
-       dialog
-   </div>
+    return <div className={style.wrapper}>
+        <div className={style.dialogs}>
+            <p>Batman</p>
+            <p>Pavel Durov</p>
+            <p>Ilon Mask</p>
+            <p>Den Abramov</p>
+        </div>
+        <div className={style.messages}>
+            <p>Hi</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consequuntur dolor eos hic illo laborum modi optio pariatur quisquam
+                repellat?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, nostrum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo in numquam odio quas?</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    </div>
 };
 
 export default Dialogs;
