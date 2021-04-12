0c2cedde966931b22f5e1a61a9e7817371fd01c4
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-04-12 21:47:31 +0300

    19.Browser Routing, add route from components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Header from "./Components/Header/Header";
 import Navbar from "./Components/Nav/Navbar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import {Route} from 'react-router-dom';
+import Settings from "./Components/Settings/Settings";
+import Music from "./Components/Music/Music";
+import News from "./Components/News/News";
 
 function App() {
     return (
@@ -11,8 +15,11 @@ function App() {
             <Header/>
             <Navbar/>
             <div className={'app-content'}>
-                {/*<Profile/>*/}
-                <Dialogs/>
+                <Route path={'/profile'} render={() => <Profile/>}/>
+                <Route path={'/dialogs'} render={() => <Dialogs/>}/>
+                <Route path={'/news'} render={() => <News/>}/>
+                <Route path={'/settings'} render={() => <Settings/>}/>
+                <Route path={'/music'} render={() => <Music/>}/>
             </div>
         </div>
     );
--- src/Components/Nav/Navbar.tsx
+++ src/Components/Nav/Navbar.tsx
@@ -4,12 +4,11 @@ import style from './Navbar.module.css'
 const Navbar = () => {
     return <nav className={style.nav}>
         <ul>
-            <li><a href="">Profile</a></li>
-            <li><a href="">Message</a></li>
-            <li><a href="">News</a></li>
-            <li><a href="">Settings</a></li>
-            <li><a href="">Music</a></li>
-            <li><a href="">Settings</a></li>
+            <li><a href="/profile">Profile</a></li>
+            <li><a href="/dialogs">Dialogs</a></li>
+            <li><a href="/news">News</a></li>
+            <li><a href="/settings">Settings</a></li>
+            <li><a href="/music">Music</a></li>
         </ul>
     </nav>
 }
