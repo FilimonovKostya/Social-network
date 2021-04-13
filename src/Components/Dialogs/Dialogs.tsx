6b6ec31653b52c7b29ae05f11109aff0ae0b7d54
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-04-13 22:02:18 +0300

    24.Bll and Ui

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -4,6 +4,35 @@ import DialogItem from "./DialogItem/DialogItem";
 import MessageItem from "./MessageItem/MessageItem";
 
 const Dialogs = () => {
+
+    type DialogsItemsType = {
+        id: number
+        name: string
+    }
+    const dialogItems: DialogsItemsType[] = [
+        {
+            id: 1,
+            name: 'Batman'
+        },
+        {
+            id: 2,
+            name: 'Pavel Durov'
+        },
+        {
+            id: 3,
+            name: 'Ilon Mask'
+        },
+        {
+            id: 4,
+            name: 'Den Abramov'
+        },
+        {
+            id: 5,
+            name: 'Filimonov Kostya'
+        },
+
+    ]
+
     return <div className={style.wrapper}>
         <div className={style.dialogs}>
             <DialogItem id={1} name={'Batman'}/>
--- src/Components/Profile/ProfileInfo/MyPosts/MyPosts.tsx
+++ src/Components/Profile/ProfileInfo/MyPosts/MyPosts.tsx
@@ -4,6 +4,31 @@ import Post from "../Post/Post";
 import MyPost from './MyPost/MyPost';
 
 const MyPosts = () => {
+
+    type PostType = {
+        message: string
+        avatarImg: string
+        likes: number
+    }
+    const posts: PostType[] = [
+        {
+            message: 'Chose React or Vue ?',
+            avatarImg: '',
+            likes: 2
+        },
+        {
+            message: 'React awesome library, in my opinion',
+            avatarImg: '',
+            likes: 100
+        },
+        {
+            message: 'No, Vanila Js is the best framework',
+            avatarImg: '',
+            likes: -20
+        },
+
+    ]
+
     return <div className={style.myPosts}>
         <MyPost/>
         <Post message={'Chose React or Vue ?'} avatarImg={''} likes={2}/>
