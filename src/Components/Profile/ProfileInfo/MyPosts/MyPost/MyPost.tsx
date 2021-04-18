0aab7fb1ad5db291a25ad3af070a524c2cb50d11
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-04-18 21:46:36 +0300

    31.Virtual dom , onChange textareaValue

--- src/Components/Profile/ProfileInfo/MyPosts/MyPost/MyPost.tsx
+++ src/Components/Profile/ProfileInfo/MyPosts/MyPost/MyPost.tsx
@@ -2,12 +2,16 @@ import style from "./MyPost.module.css";
 import React from "react";
 
 const MyPost = () => {
+
+    const textAreaValue = React.createRef<HTMLTextAreaElement>()
+    const onChangeTextArea = () => alert(textAreaValue.current?.value)
+
     return <>
         <h4>My posts</h4>
         <label className={style.formInputContainer}>
-            <textarea className={style.formInput} id="word-count-input"/>
+            <textarea className={style.formInput} id="word-count-input" ref={textAreaValue} />
             <span className={style.formInputLabel}/>
-            <button className={style.button}>Send</button>
+            <button className={style.button} onClick={onChangeTextArea}>Send</button>
         </label>
     </>
 }
