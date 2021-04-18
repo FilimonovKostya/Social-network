953a03644ba1d64a81d408a15251431c34a62453
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-04-18 22:12:55 +0300

    31.Add  function on post

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -11,12 +11,19 @@ const Dialogs = ({dialogsData}: DialogsPropsType) => {
     const dialogsItems = dialogsData.dialogs.map((el) => <DialogItem key={el.id} id={el.id} name={el.name}/>)
     const messagesItem = dialogsData.messages.map((el) => <MessageItem key={el.id} message={el.message}/>)
 
+    const textAreaValue = React.createRef<HTMLTextAreaElement>()
+    const onChangeTextArea = () => alert(textAreaValue.current?.value)
+
     return <div className={style.wrapper}>
         <div className={style.dialogs}>
             {dialogsItems}
         </div>
         <div className={style.messages}>
             {messagesItem}
+        </div>
+        <div className={style.textArea}>
+            <textarea name="" id="" cols={30} rows={10} ref={textAreaValue}/>
+            <button className={style.button} onClick={onChangeTextArea}>Send</button>
         </div>
     </div>
 };
