527619229de4d759270d386391b3c1f7af76ae2d
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-06-01 23:47:42 +0300

    55.made pagination (intermediate)

--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {UsersType} from "../../Redux/usersReducer";
 import style from './Users.module.css'
 import axios from "axios";
@@ -11,17 +11,29 @@ type UsersPropsType = {
 }
 
 const Users = ({users, follow, unFollow, setUsers}: UsersPropsType) => {
+    const [pagination, setPagination] = useState([1])
+    const [counterPagination, setCounterPagination] = useState(1)
 
     useEffect(() => {
 
-            axios.get<{ items: UsersType[] }>('https://social-network.samuraijs.com/api/1.0/users')
-                .then((response) => {
-                    setUsers(response.data.items)
-                    console.log(response.data.items)
-                })
-    }, [])
+        axios.get<{ items: UsersType[] }>(`https://social-network.samuraijs.com/api/1.0/users?page=${counterPagination}`)
+            .then((response) => {
+                setUsers(response.data.items)
+                console.log(response.data)
+                let pages = []
+                debugger
+                // @ts-ignore
+                for (let i = 0; i <= response.data.totalCount; i++) {
+                    pages[i] = i
+                    console.log({i})
+                }
+                console.log(pages)
+                setPagination(pages)
+            })
+    }, [counterPagination])
 
     return <div className={style.container}>
+        {pagination.map((el, index) => <span key={index} onClick={() => setCounterPagination(index)}>{el}</span>)}
         {
             users.map(el => <>
                 <div className={style.avatarIcon} key={el.id}><img
