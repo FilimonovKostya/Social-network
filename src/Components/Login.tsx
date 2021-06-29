9a94202a98a75944f1b7a85fa4ef2fc6cb4e4361
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-06-29 20:57:34 +0300

    75. add formik

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,15 +1,8 @@
 import React from 'react'
 
-import { useForm, SubmitHandler } from "react-hook-form/dist";
 
-type Inputs = {
-    example: string,
-    exampleRequired: string,
-};
 
 const Login = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
     return <div>
         <h4>LOGIN</h4>
