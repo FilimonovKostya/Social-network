a4465a6e9102962be1cebdded68dfb2aa90079b1
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-06-29 23:41:22 +0300

    75.Hook form work

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,30 +1,24 @@
 import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
-type Inputs = {
-    example: string,
-    exampleRequired: string,
-};
+type IFormInput = {
+    login: string;
+    password: string;
+    rememberMe: boolean;
+}
 
 
 const Login = () => {
-
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
-
-    console.log(watch("example")) // watch input value by passing the name of it
-
+    const { register, handleSubmit, formState: {errors} } = useForm<IFormInput>();
+    const onSubmit = handleSubmit((data) => console.log(data))
+    console.log('re',register)
     return (
-        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-        <form onSubmit={handleSubmit(onSubmit)}>
-            {/* register your input into the hook by invoking the "register" function */}
-            <input defaultValue="test" {...register("example")} />
+        <form onSubmit={onSubmit}>
+            <input {...register("login", { required: true, minLength: 3 })} />
+            {errors.login && 'Require'}
 
-            {/* include validation with required or other standard HTML validation rules */}
-            <input {...register("exampleRequired", { required: true })} />
-            {/* errors will return when field validation fails  */}
-            {errors.exampleRequired && <span>This field is required</span>}
-
+            <input type={'password'} {...register("password", {required: true, minLength: 5})} />
+            <input type="checkbox" {...register("rememberMe", {required: false })} />
             <input type="submit" />
         </form>
     );
