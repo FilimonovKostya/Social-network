7d1bc975ab6ee4b2ae7b2887a7337a35b9bfa19f
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-06-29 20:55:25 +0300

    75. Remove hook form

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -14,16 +14,9 @@ const Login = () => {
     return <div>
         <h4>LOGIN</h4>
 
-        <form onSubmit={handleSubmit(onSubmit)}>
-            {/* register your input into the hook by invoking the "register" function */}
-            <input defaultValue="test" {...register("example")} />
-
-            {/* include validation with required or other standard HTML validation rules */}
-            <input {...register("exampleRequired", { required: true })} />
-            {/* errors will return when field validation fails  */}
-            {errors.exampleRequired && <span>This field is required</span>}
-
-            <input type="submit" />
+        <form>
+            <input type="text" name={'Login'}/>
+            <input type="text" name={'password'}/>
         </form>
     </div>
 };
