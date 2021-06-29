1a815fb8587e451cc8493c02ff1cb36bf96a3f02
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-06-29 22:14:55 +0300

    75.Hook form work

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
+type Inputs = {
+    example: string,
+    exampleRequired: string,
+};
 
 
 const Login = () => {
 
-    return <div>
-        <h4>LOGIN</h4>
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
-        <form>
-            <input type="text" name={'Login'}/>
-            <input type="text" name={'password'}/>
+    console.log(watch("example")) // watch input value by passing the name of it
+
+    return (
+        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+        <form onSubmit={handleSubmit(onSubmit)}>
+            {/* register your input into the hook by invoking the "register" function */}
+            <input defaultValue="test" {...register("example")} />
+
+            {/* include validation with required or other standard HTML validation rules */}
+            <input {...register("exampleRequired", { required: true })} />
+            {/* errors will return when field validation fails  */}
+            {errors.exampleRequired && <span>This field is required</span>}
+
+            <input type="submit" />
         </form>
-    </div>
+    );
 };
 
 export default Login;
