10cca1b5397b7db12f2c391154ade4169b4cfcd8
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-07-01 23:32:21 +0300

    79. fix layout header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,17 +3,28 @@ import style from './Header.module.css'
 import {NavLink} from "react-router-dom";
 
 type HeaderPropsType = {
-    login:string
-    logoutTC:(isAuth:boolean) => void
+    login: string
+    logoutTC: (isAuth: boolean) => void
 
 }
 
 const Header = ({login, logoutTC}: HeaderPropsType) => {
-    console.log('login', login )
+    console.log('login', login)
 
     return <header className={style.header}>
-        <img src="https://cdn.worldvectorlogo.com/logos/react-2.svg" alt="logo"/>
-        {login ? <button onClick={() => logoutTC(false)}> Logout  </button> : <NavLink to={'/login'}> Name: {login}  Login </NavLink>}
+        <div className={style.wrapper}>
+            <div className={style.avatar}>
+                <img src="https://pm1.narvii.com/6802/b775c9e1266f61a61737324b45e0975ed22d7328v2_hq.jpg" alt="person"/>
+                <div className={style.info}>
+                    <h3>Filimomov Kostya</h3>
+                    <button>Log out</button>
+                </div>
+            </div>
+
+        </div>
+        <div className={style.logo}> <img src="https://cdn.worldvectorlogo.com/logos/react-2.svg" alt="logo"/></div>
+
+        {/*{login ? <button onClick={() => logoutTC(false)}> Logout  </button> : <NavLink to={'/login'}> Name: {login}  Login </NavLink>}*/}
     </header>
 }
 
