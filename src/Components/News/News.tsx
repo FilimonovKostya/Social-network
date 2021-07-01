37516a2db26ce7ed3fa7a5b6b6e67999146fba4a
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-07-02 00:23:44 +0300

    79. fix card layout

--- src/Components/News/News.tsx
+++ src/Components/News/News.tsx
@@ -4,16 +4,18 @@ import style from './Card.module.css'
 const News = () => {
     return <div className={style.wrapper}>
         <div className={style.profil}>
-            <div className={style.kapak}/>
-            <img className={style.devrim} src="https://pm1.narvii.com/6802/b775c9e1266f61a61737324b45e0975ed22d7328v2_hq.jpg"/>
-            <a className={style.isim}>Devrim Demir</a>
-            <p className={style.turkej}>Turkey</p>
 
+            <div className={style.bgImage}/>
+      <img className={style.avatar} src="https://pm1.narvii.com/6802/b775c9e1266f61a61737324b45e0975ed22d7328v2_hq.jpg"/>
+
+            <div className={style.text}>
+                <a className={style.isim}>Devrim Demir</a>
+                <p className={style.turkej}>Turkey</p>
+            </div>
 
             <div className={style.iletisim}>
                 <a target="_blank" href="">
-                    <img draggable="false" className={style.sayfalar}
-                         src=""/></a>
+                    <img draggable="false" className={style.sayfalar} src=""/></a>
 
                 <a target="_blank" href=""><img draggable="false" className={style.sayfalar} src=""/></a>
 
