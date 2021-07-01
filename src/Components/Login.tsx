1e20d330deccba7cc4dfc9f49a41c8fea1035380
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-07-01 22:24:17 +0300

    79. Fix layout

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -18,15 +18,25 @@ const Login = () => {
 
 
     return (
-        <form onSubmit={onSubmit}>
-            <input  {...register("email", {required: true,})} />
-            {errors.email && 'Require'}
-
-            <input type={'password'} {...register("password", {required: true, minLength: 5})} />
-            {errors.password && 'Require'}
-
-            <input type="checkbox" {...register("rememberMe", {required: false})} />
-            <input type="submit"/>
+        // <form onSubmit={onSubmit}>
+        //     <input  {...register("email", {required: true,})} />
+        //     {errors.email && 'Require'}
+        //
+        //     <input type={'password'} {...register("password", {required: true, minLength: 5})} />
+        //     {errors.password && 'Require'}
+        //
+        //     <input type="checkbox" {...register("rememberMe", {required: false})} />
+        //     <input type="submit"/>
+        // </form>
+        <form className="login-form">
+            <h1>Login</h1>
+            <input type="text" name="username" placeholder="Username" required />
+                <input type="password" name="password" placeholder="Password" required />
+                    <input type="submit" name="Login" value="Login" />
+                        <div className="links">
+                            <a href="#">Forgot Password</a>
+                            <a href="#">Sign Up</a>
+                        </div>
         </form>
     );
 };
