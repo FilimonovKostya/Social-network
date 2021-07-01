9d5e4e4ea3995bcb61b4a4235e5f24f984946adc
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-07-01 23:44:16 +0300

    79. fix Preloader

--- src/Components/Preloader/Preloader.tsx
+++ src/Components/Preloader/Preloader.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import preloader from './loading.svg'
+import style from './Preloader.module.css'
 
 const Preloader = () => {
-    return <img src={preloader} alt="preloader"/>
+    return <div className={style.wrapper}><img src={preloader} alt="preloader"/></div>
 };
 
 export default Preloader;
