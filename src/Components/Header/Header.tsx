5e265573e791d2d8672b8caa2f8f107d83e69c3b
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-07-03 21:59:12 +0300

    79. Login and logout work. But not redirect and don't update status in header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import style from './Header.module.css'
 import {NavLink} from "react-router-dom";
+import style from './Header.module.css'
 
 type HeaderPropsType = {
     login: string
@@ -16,15 +16,16 @@ const Header = ({login, logoutTC}: HeaderPropsType) => {
             <div className={style.avatar}>
                 <img src="https://pm1.narvii.com/6802/b775c9e1266f61a61737324b45e0975ed22d7328v2_hq.jpg" alt="person"/>
                 <div className={style.info}>
-                    <h3>Filimomov Kostya</h3>
-                    <button className={style.btn}>Log out</button>
+                    <h3>{login}</h3>
+                    {login ? <button className={style.btn} onClick={() => logoutTC(false)}> Logout </button> :
+                        <NavLink to={'/login'}> Name: {login} Login </NavLink>}
+
                 </div>
             </div>
 
         </div>
         <div className={style.logo}><img src="https://cdn.worldvectorlogo.com/logos/react-2.svg" alt="logo"/></div>
 
-        {/*{login ? <button onClick={() => logoutTC(false)}> Logout  </button> : <NavLink to={'/login'}> Name: {login}  Login </NavLink>}*/}
     </header>
 }
 
