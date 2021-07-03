ee7ebda435950d16d4d2a7f8f7f1b9eedab0d817
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-07-03 20:45:25 +0300

    79. card user

--- src/Components/News/News.tsx
+++ src/Components/News/News.tsx
@@ -6,11 +6,21 @@ const News = () => {
         <div className={style.profil}>
 
             <div className={style.bgImage}/>
-      <img className={style.avatar} src="https://pm1.narvii.com/6802/b775c9e1266f61a61737324b45e0975ed22d7328v2_hq.jpg"/>
+            <img className={style.avatar} src="https://pm1.narvii.com/6802/b775c9e1266f61a61737324b45e0975ed22d7328v2_hq.jpg"/>
 
             <div className={style.text}>
-                <a className={style.isim}>Devrim Demir</a>
-                <p className={style.turkej}>Turkey</p>
+                <div>
+                    <p>status :</p>
+                    <p>Name :</p>
+                    <p>Age :</p>
+                    <p>Web Site:</p>
+                </div>
+                <div>
+                    <p>Free</p>
+                    <p>Filimonov Kostya</p>
+                    <p>24</p>
+                    <p>Github</p>
+                </div>
             </div>
 
             <div className={style.iletisim}>
@@ -19,7 +29,7 @@ const News = () => {
 
                 <a target="_blank" href=""><img draggable="false" className={style.sayfalar} src=""/></a>
 
-                <a target="_blank" href="https://www.facebook.com/devrimos"><img draggable="false" className={style.sayfalar} src=""/></a>
+                <a target="_blank" href="https://www.facebook.com/devrimos"><img className={style.sayfalar} src="https://is5-ssl.mzstatic.com/image/thumb/Purple125/v4/9e/7d/ec/9e7dec5a-a2f3-a348-3ef0-04ba0797138b/Icon-Production-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png"/></a>
 
                 <a target="_blank" href="https://www.instagram.com/devrim0s"><img draggable="false" className={style.sayfalar} src=""/></a>
 
