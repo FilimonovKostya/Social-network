e7971ee785faf7583a779118b3c40d63b094cc73
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-07-28 21:42:09 +0300

    fix layout

--- src/Components/Profile/ProfileInfo/PersonalInfo/PersonalInfo.tsx
+++ src/Components/Profile/ProfileInfo/PersonalInfo/PersonalInfo.tsx
@@ -15,7 +15,6 @@ type PersonalInfoPropsType = {
 }
 
 const PersonalInfo = ({userProfile, updateProfile, status, changeStatus, updatePhoto}: PersonalInfoPropsType) => {
-    console.log('userPr', userProfile.contacts)
 
     const {register, handleSubmit} = useForm<any>();
     const onSubmit: SubmitHandler<any> = handleSubmit((data) => {
@@ -28,6 +27,7 @@ const PersonalInfo = ({userProfile, updateProfile, status, changeStatus, updateP
     }
 
     const userProfileContacts = Object.keys(userProfile.contacts ?? {})
+    console.log('userProfileContacts', userProfileContacts)
 
     return <div className={style.wrapperPersonalInfo}>
         <div className={style.wrapperPhotoProfile}>
@@ -37,27 +37,23 @@ const PersonalInfo = ({userProfile, updateProfile, status, changeStatus, updateP
                 <input className={style.input} type="file" id={'updatePhoto'} onChange={onUpdatePhoto}/>
             </label>
         </div>
-        <div className={style.personalInfo}><h3>{userProfile?.fullName}</h3>
-            <p>Looking for a job: <span>{userProfile?.lookingForAJob ? 'Ищу работу за шаурму' : 'Ищу за деньги'}</span>
-            </p>
-            <Status status={status} changeStatus={changeStatus}/>
-            <p>Web Site: <span>{userProfile.contacts && ''}</span></p>
-            <p>Description for Job: {userProfile.lookingForAJobDescription}</p>
+        <form onSubmit={onSubmit} className={style.wrapperContacts}>
+            <div className={style.leftInfo}>
+                <p>Looking for a job : <span>{userProfile?.lookingForAJob ? 'В поиске работы' : 'Не рассматриваю' +
+                    ' предложения'}</span>
+                </p>
+                <Status status={status} changeStatus={changeStatus}/>
+                <p>Web Site: <span>{userProfile.contacts && ''}</span></p>
+                <p>Description for Job: {userProfile.lookingForAJobDescription}</p>
+            </div>
+            <div style={{color: 'black'}}>
+                {userProfileContacts.map((key) => <>
 
-            {/*{userProfileContacts.map((key) => {*/}
-            {/*    return <form onSubmit={onSubmit}>*/}
-
-            {/*        <Contact register={register} handleSubmit={handleSubmit} contactTitle={key as SocialMediaType}*/}
-            {/*                 titleValue={userProfile.contacts[key as SocialMediaType]}/>*/}
-
-            {/*        <button>Send</button>*/}
-
-            {/*    </form>*/}
-
-            {/*})} */}
-
-
-        </div>
+                    <Contact register={register} handleSubmit={handleSubmit} contactTitle={key as SocialMediaType}
+                             titleValue={userProfile.contacts[key as SocialMediaType]}/>
+                </>)}
+            </div>
+        </form>
     </div>
 }
 
